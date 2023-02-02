57bce5ae1345dba627721ec22cd0fcf408cf9152
santiago espinosa <sanespinosa10@gmail.com>
2023-02-02 19:36:30 -0300

    initial commit

---
+++ app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavComponent } from './components/nav/nav.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavComponent,
+    ExperienciaComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
